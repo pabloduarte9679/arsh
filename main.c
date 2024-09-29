0e619fb42e87ba6b2416d52920e8186ad25af733
pabloduarte9679 <elierduarte344@gmail.com>
2024-09-29 01:55:04 -0600

    base source file

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
+void prompt(char *username, char *hostname, char *currentdir);
+void mainloop();
 int main(void){
+  char *buffer;
+  buffer = getlogin();
+  printf("%s\n", buffer);
   return 0;
 }
+
+void prompt(char *username, char *hostname, char *currentdir){
+  printf("$%s@%s->%s ");
+}
+
+void mainloop(){}
+
