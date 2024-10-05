4427053e8f58a48dcfcabd3022a6dcd4b990fedd
pabloduarte9679 <elierduarte344@gmail.com>
2024-10-05 14:10:36 -0600

    base structure of the shell done

--- main.c
+++ main.c
@@ -1,19 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#define BUFSIZE 1024
 
-void prompt(char *username, char *hostname, char *currentdir);
-void mainloop();
+void relativepath(char *dir, char *rdir){
+  char c;
+  int sbs = 0;
+  char tmp[strlen(rdir)];
+  for(int i = strlen(dir); i >= 0; i--){
+    if(dir[i] != '/'){
+      tmp[sbs++] = dir[i];
+    }else{
+      break;
+    }
+  }
+  for(int i = 0; i < sbs; i++){
+    rdir[i] = tmp[sbs - i - 1];
+  }
+  rdir[sbs] = '\0';
+}
+
+void mainloop(char *usr, char *hostname, char *homedir){
+  while(1){
+    char *buffer = NULL;
+    char dir[40];
+    char rdir[20] = {' '};
+    getcwd(dir, 40);
+    if(strcmp(homedir, dir) == 0){
+      dir[0] = '~';
+      dir[1] = '\0';
+    }
+    relativepath(dir, rdir);
+    printf("[%s@%s %s]$ ", usr, hostname, rdir);
+    size_t size = BUFSIZE;
+    getline(&buffer, &size, stdin);
+    char cmp[2] = {"\n\0"};
+    if(strcmp(buffer, cmp) != 0){
+     printf("%s", buffer); 
+    }
+    
+    free(buffer);
+  }
+}
+
 int main(void){
-  char *buffer;
-  buffer = getlogin();
-  printf("%s\n", buffer);
+  char *username = getlogin();
+  char hostname[20];
+  gethostname(hostname, 20);
+  char *homedir;
+  homedir = getenv("HOME");
+  mainloop(username, hostname, homedir);
   return 0;
 }
-
-void prompt(char *username, char *hostname, char *currentdir){
-  printf("$%s@%s->%s ");
-}
-
-void mainloop(){}
-
